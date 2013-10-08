2c51a82690ff9c24714095b4a6852405b91b0349
Juanpe <juanpedro.catalan@thinksmart.es>
2013-10-08 16:49:08 +0200

    Version 1.0

--- JPSRTParser/JPSRTParser/JPSRTParser.h
+++ JPSRTParser/JPSRTParser/JPSRTParser.h
@@ -8,6 +8,43 @@
 
 #import <Foundation/Foundation.h>
 
+/*
+ * Posted when SRT file has been downloaded.
+ */
+extern NSString * const JPSRTFileAvailableNotification;
+
+/*
+ * Posted when SRT file can't load.
+ */
+extern NSString * const JPErrorGetSRTFileNotification;
+
+/*
+ * Posted when SRT parsing process fail
+ */
+extern NSString * const JPErrorParsingSRTNotification;
+
+typedef NS_ENUM(NSUInteger, JPSRTParsingState) {
+    JPSRTParsingStateCompleted,
+    JPSRTParsingStateFailed
+};
+
+typedef void (^JPSRTParserCompletionBlock)(JPSRTParsingState status, NSArray* subtitles);
+
 @interface JPSRTParser : NSObject
+
+#pragma mark - Properties
+@property (nonatomic, copy)     NSString* srtFilePath;
+@property (nonatomic, copy)     NSURL* srtURL;
+@property (nonatomic, copy)     NSString* srtStringValue;
+@property (nonatomic, copy)     NSDateFormatter* dateFormatter;;
+@property (nonatomic, copy)     JPSRTParserCompletionBlock completionBlock;
+@property (nonatomic, assign)   NSStringEncoding srtEncodeType;
+
+#pragma mark - Methods
+- (id) initWithContentOfSRTFile:(NSString *) srtFilePath;
+- (id) initWithContentOfSRTURL:(NSURL *) srtURL;
+
+- (NSArray *) parse;
+- (void) parseCompletion:(JPSRTParserCompletionBlock)completionBlock;
 
 @end
